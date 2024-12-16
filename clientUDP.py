79de528d6bc8eed80c0db87d66cf058e870e6ccf
Екатерина Митруева <mitruevak@gmail.com>
2024-12-16 20:23:46 +0800

    Перезаписана папка 2laba

--- clientUDP.py
+++ clientUDP.py
@@ -3,11 +3,11 @@ import socket
 # Создаем UDP-сокет
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Адрес и порт сервера
+# адрес и порт сервера
 server_address = ('localhost', 12345)
 
-# Отправляем сообщение серверу
-message = "Привет, UDP-сервер!"
+# отправляем сообщение серверу
+message = "— Побудь здесь. Если там неопасно, я свистну. \n — А если опасно? \n — Заору."
 client_socket.sendto(message.encode('utf-8'), server_address)
 print(f"Отправлено сообщение: {message}")
 
@@ -15,6 +15,6 @@ print(f"Отправлено сообщение: {message}")
 data, server = client_socket.recvfrom(1024)
 print(f"Получено сообщение от сервера: {data.decode('utf-8')}")
 
-# Закрываем сокет
+# закрываем сокет
 client_socket.close()
 print("Соединение закрыто.")
--- serverUDP.py
+++ serverUDP.py
@@ -1,22 +1,22 @@
 import socket
 
-# Создаем UDP-сокет
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# создаем UDP-сокет
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #AF_INET указывает на использование IPv4, а SOCK_STREAM на UDP
 
-# Привязываем сокет к адресу и порту
+# привязываем сокет к адресу и порту
 server_address = ('localhost', 12345)  # Используем localhost и порт 12345
 server_socket.bind(server_address)
 print("UDP-сервер запущен и ожидает данных на порту 12345...")
 
 while True:
-    # Ожидаем данные от клиента
-    data, client_address = server_socket.recvfrom(1024)  # Максимальный размер данных 1024 байта
+    # ожидаем данные от клиента
+    data, client_address = server_socket.recvfrom(1024)  # устанавливаем максимальный размер получаемых данных 1024 байта
     print(f"Получено сообщение от клиента {client_address}: {data.decode('utf-8')}")
 
-    # Отправляем данные обратно клиенту
+    # отправляем данные обратно клиенту
     server_socket.sendto(data, client_address)
     print(f"Отправлено сообщение обратно клиенту {client_address}")
 
-    # Закрываем сокет (если нужно завершить работу сервера)
-    # server_socket.close()
-    # break
+    # закрываем сокет 
+    server_socket.close()
+    break
